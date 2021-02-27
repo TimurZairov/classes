8775e7e93e9f74fbf686f51c742a5bedbe2597ad
Timur Zairov <zairovne@gmail.com>
2021-02-27 16:07:50 +0500

    classes

---
+++ js.js
@@ -0,0 +1,18 @@
+'use-strict';
+
+class First {
+    hello(){
+        console.log('Привет я метод родителя!');
+    }
+}
+class Second extends First{
+
+    hello(){
+        first.hello();
+        console.log('А я наследуемый метод!');
+    }
+}
+
+const first = new First();
+const second = new Second();
+second.hello();
